3cb9ef3d3602888f25bfaa74fb11aa3d1651990e
OKI KUS MAHESA <43431437+L200170064@users.noreply.github.com>
2019-07-08 20:23:06 +0700

    Update Modul 10.py

--- Modul10/Modul 10.py
+++ Modul10/Modul 10.py
@@ -1,97 +1,30 @@
-#Nomer1 membuat fingsi ukuranPohon(akar) yang akan mendapat ukuran pohon biner
-class simpulPohonBiner():
-    def __init__(self,data):
-        self.data = data
-        self.kiri = None
-        self.kanan = None
+import timeit
+import matplotlib.pyplot as plt
 
-def ukuranPohon (akar):
-    ukuran = 0
-    if akar is not None :
-        if akar.kiri is None and akar.kanan is None :
-            ukuran += 1
-        else :
-            hasil =  ukuranPohon (akar.kiri)
-            ukuran += hasil
-            hasil = ukuranPohon(akar.kanan)
-            ukuran += hasil
-    return ukuran
+## Ini fungsi nested loop yang akan diuji:
+def kalangBersusuh(n):
+     for i in range(n):
+         for j in range(n):
+              i+j
 
-A = simpulPohonBiner ("Ambarawa")
-B = simpulPohonBiner ("Bantul")
-C = simpulPohonBiner ("Denpasar")
-D = simpulPohonBiner ("Palembang")
-E = simpulPohonBiner ("Banten")
-F = simpulPohonBiner ("Garut")
-G = simpulPohonBiner ("Jakarta")
-H = simpulPohonBiner ("Flores")
-I = simpulPohonBiner ("Indramayu")
-J = simpulPohonBiner ("Halmahera Timur")
+## Ini fungsi pengujinya:
+def ujiKalangBersusuh(n):
+    ls=[]
+    jangkauan = range(1,n+1)
+    siap = "from __main__ import kalangBersusuh"
+    for i in jangkauan:
+        #print(’i =’,i) # baris ini bisa dihidupkan atau dimatikan
+        t=timeit.timeit("kalangBersusuh(" + str(i) +")", setup=siap, number=1)
+        ls.append(t)
+    return ls
 
+## Pemanggilan pengujian
+n = 1000
+LS = ujiKalangBersusuh(n) # dari 1 sampai 1000.
+## LS adalah list hasil uji kecepatan, dari n sedikit ke banyak
 
-A.kiri = B; A.kanan = C
-B.kiri = D; B.kanan = E
-C.kiri = F; C.kanan = G
-E.kiri = H
-G.kanan = I
-
-#Nomer 2 membuat fungsi tinggi pohon(akar) yg akan mendapat ketinggian sebuah pohon biner
-class simpulPohonBiner():
-    def __init__(self,data):
-        self.data = data
-        self.kiri = None
-        self.kanan = None
-
-def tinggiPohon(akar):
-    if akar is None :
-        return 0
-    else:
-        kiri = tinggiPohon(akar.kiri)
-        kanan = tinggiPohon(akar.kanan)
-        if kiri > kanan:
-            return kiri+1
-        else:
-            return kanan+1
-
-A = simpulPohonBiner ("Ambarawa")
-B = simpulPohonBiner ("Bantul")
-C = simpulPohonBiner ("Denpasar")
-D = simpulPohonBiner ("Palembang")
-E = simpulPohonBiner ("Banten")
-F = simpulPohonBiner ("Garut")
-G = simpulPohonBiner ("Jakarta")
-H = simpulPohonBiner ("Flores")
-I = simpulPohonBiner ("Indramayu")
-J = simpulPohonBiner ("Halmahera Timur")
-
-#3 membuat fungsi cetakDataDanLevel()
-
-class simpulPohonBiner():
-    def __init__(self,data):
-        self.data = data
-        self.kiri = None
-        self.kanan = None
-
-def cetakDataDanLevel (akar, level=-1):
-    level += 1
-    if akar is not None:
-        print akar.data, "Level", level
-        cetakDataDanLevel (akar.kiri, level)
-        cetakDataDanLevel (akar.kanan,level)
-
-A = simpulPohonBiner ("Ambarawa")
-B = simpulPohonBiner ("Bantul")
-C = simpulPohonBiner ("Denpasar")
-D = simpulPohonBiner ("Palembang")
-E = simpulPohonBiner ("Banten")
-F = simpulPohonBiner ("Garut")
-G = simpulPohonBiner ("Jakarta")
-H = simpulPohonBiner ("Flores")
-I = simpulPohonBiner ("Indramayu")
-J = simpulPohonBiner ("Halmahera Timur")
-
-A.kiri = B; A.kanan = C
-B.kiri = D; B.kanan = E
-C.kiri = F; C.kanan = G
-E.kiri = H
-G.kanan = I
+## Menggambar grafik. Di bawah ini saja yang diulang saat me-nyetel skala
+plt.plot(LS) # Mem-plot hasil uji
+skala = 7700000 # <------ Setel skala ini sesuai hasilmu
+plt.plot([x*x/skala for x in range(1,n+1)]) # Grafik x^2 untuk pembanding.
+plt.show() # Tunjukkan plotnya
